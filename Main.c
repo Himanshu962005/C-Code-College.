1392bc7825f9d9c390165cb2c26f5c3a4d14eabc
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-03-10 14:08:15 +0530

    Add files via upload

---
+++ Main.c
@@ -0,0 +1,10 @@
+// This is my first 'C' program.
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    system("cls");
+    printf("My Name is Himanshu Legendary Blader.");
+    printf("\nMy Roll No = 962005.");
+    printf("\nMy Class is BCA 2nd Semester.");
+}
