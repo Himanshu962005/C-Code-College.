2671a14edf190475466d39087fefbd81b360c9a8
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-03-11 16:57:38 +0530

    Add files via upload

---
+++ Main2.c
@@ -0,0 +1,13 @@
+// Printing Values of Variables.
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    char Gender = 'M';
+    int N = 7;
+    float PER = 79.28;
+    system("cls");
+    printf("My Gender = %C", Gender);
+    printf("\nMy Roll Number = %d", N);
+    printf("\nMy Percentage = %f", PER);
+}
