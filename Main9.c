36b555b2a1207739b9a15de1e80ee8a7042f0f45
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-03-17 16:22:50 +0530

    Add files via upload

---
+++ Main9.c
@@ -0,0 +1,12 @@
+// Circumference of a Circle.
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int r = 5;
+    float cf;
+    system("cls");
+    printf("Radius of the Circle = %d", r);
+    cf = (float)2 * 22 / 7 * r;
+    printf("\nCircumference of the Circle = %f", cf);
+}
