dc78b2f18ee1cebd98d67e30e4f285fcc4689d7d
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-03-17 14:41:20 +0530

    Add files via upload

---
+++ Main7.c
@@ -0,0 +1,12 @@
+// Area of rectangle.
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int l = 10, w = 20, ar;
+    system("cls");
+    printf("Length of rectangle = %d", l);
+    printf("\nWidth of rectangle = %d", w);
+    ar = l * w;
+    printf("\nArea of rectangle = %d", ar);
+}
