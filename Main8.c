0f8dac28aea8d2a8138fb96a777daa3b4923a14a
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-03-17 15:59:28 +0530

    Add files via upload

---
+++ Main8.c
@@ -0,0 +1,12 @@
+// Area of a circle.
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int r = 5;
+    float ar;
+    system("cls");
+    printf("Radius of circle = %d", r);
+    ar = (float)22 / 7 * r * r;
+    printf("\nArea of a circle = %f", ar);
+}
