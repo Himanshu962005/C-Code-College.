c7977bc67dc64224633110e89e1ebf860a28638a
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-03-20 22:26:56 +0530

    Add files via upload

---
+++ Main15.c
@@ -0,0 +1,19 @@
+// Testing Positive and Negative Numbers.
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int n;
+    system("cls");
+    printf("Input a number = ");
+    scanf("%d", &n);
+    if (n > 0)
+    {
+        printf("The number is positive.");
+    }
+    else
+    {
+        printf("The number is negative.");
+    }
+    printf("\nHimanshu Legendary Blader");
+}
