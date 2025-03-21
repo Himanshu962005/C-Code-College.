f9ae359bb65bcb19cd6593d6500a99bd6716d383
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-03-21 17:20:51 +0530

    Add files via upload

---
+++ Main16.c
@@ -0,0 +1,21 @@
+// Finding Bigger Between Two Numbers.
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int a, b;
+    system("cls");
+    printf("Input First Number = ");
+    scanf("%d", &a);
+    printf("Input Second Number = ");
+    scanf("%d", &b);
+    if (a > b)
+    {
+        printf("First Number is Bigger ");
+    }
+    else
+    {
+        printf("Second Number is Bigger ");
+    }
+    printf("\nHimanshu Legendary Blader");
+}
