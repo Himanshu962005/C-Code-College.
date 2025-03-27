7e1cc1c3dd8962e0c5f822b8b229a0a180fcc758
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-03-27 22:05:19 +0530

    Add files via upload

---
+++ Main29.c
@@ -0,0 +1,16 @@
+// Example of while loop.
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int CNT;
+    system("cls");
+    printf("Example of while loop");
+    CNT = 1;
+    while (CNT <= 5)
+    {
+        printf("\nBCA");
+        CNT++;
+    }
+    printf("\nGood Day");
+}
