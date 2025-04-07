92e29598884c244681a2ef140cb9ca170fa11bb8
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-04-07 17:25:49 +0530

    Add files via upload

---
+++ Main46.c
@@ -0,0 +1,15 @@
+// Example of 1-Dimensional Array.
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int N[5] = {22, 88, 77, 66, 99};
+    int I;
+    system("cls");
+    printf("Example of 1-D array = ");
+    for (I = 0; I <= 4; I++)
+    {
+        printf("%d", N[I]);
+    }
+    printf("\n Good Day");
+}
