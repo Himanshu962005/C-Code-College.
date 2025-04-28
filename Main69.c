2f195564d6bd7193de749f5e302102cc3a66041d
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-04-28 11:15:38 +0530

    Add files via upload

---
+++ Main69.c
@@ -0,0 +1,11 @@
+// Symbol to ASCII.
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int ASC;
+    system("cls");
+    printf("Input an ASCII code = ");
+    scanf("%d", &ASC);
+    printf("Its equivalent symbol = %c", ASC);
+}
