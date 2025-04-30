79cd52371fd44d2dcc9166309c835418cc4fd733
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-04-30 17:58:46 +0530

    Add files via upload

---
+++ Main76.c
@@ -0,0 +1,14 @@
+// String Length.
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+void main()
+{
+    char STR[10];
+    int L;
+    system("cls");
+    printf("Input a string = ");
+    scanf("%s", STR);
+    L = strlen(STR);
+    printf("Length of the string = %d", L);
+}
