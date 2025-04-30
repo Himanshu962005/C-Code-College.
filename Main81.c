f20f9ade44eebe3037f3ed85908e4af2218046ac
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-04-30 20:04:14 +0530

    Add files via upload

---
+++ Main81.c
@@ -0,0 +1,26 @@
+// Function with passing No value, No return value.
+#include <stdio.h>
+#include <conio.h>
+void dline()
+{
+    printf("\n--------------------------------------------------");
+}
+void star()
+{
+    printf("\n************************************************");
+}
+void main()
+{
+    system("cls");
+    star();
+    dline();
+    printf("\n Welcome to C Programming Language");
+    dline();
+    star();
+    printf("\n This is a function with No value and No return value.");
+    star();
+    dline();
+    printf("\n BCA 2nd Semester 2025");
+    dline();
+    star();
+}
