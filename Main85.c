f5af9395dad580e56eba59c93702e513e0680b12
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-04-30 23:26:05 +0530

    Add files via upload

---
+++ Main85.c
@@ -0,0 +1,25 @@
+// Calculate the area and circumference of the circle.
+#include <stdio.h>
+#include <conio.h>
+void Are(int r)
+{
+    float AR;
+    AR = (float)22 / 7 * r * r;
+    printf("Area of a circle = %f", AR);
+}
+void Circum(int r)
+{
+    float CF;
+    CF = (float)2 * 22 / 7 * r;
+    printf("\nCircumference of circle = %f", CF);
+}
+void main()
+{
+    int R;
+    system("cls");
+    printf("Radius of circle = ");
+    scanf("%d", &R);
+    Are(R);
+    Circum(R);
+    printf("\nGood Day");
+}
