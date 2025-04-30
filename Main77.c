17b5240a8e5cf6898eb2aff24a22173cb9b5508a
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-04-30 18:03:58 +0530

    Add files via upload

---
+++ Main77.c
@@ -0,0 +1,13 @@
+// String Copy.
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+void main()
+{
+    char STR[10], VTR[10];
+    system("cls");
+    printf("Input a string = ");
+    scanf("%s", STR);
+    strcpy(VTR, STR);
+    printf("\n Copied string = %s", VTR);
+}
