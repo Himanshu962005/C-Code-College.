e8036acb07a0128b83480bc4a9171ea3e8d1c36a
HIMANSHU KARWA <valthimanshu@gmail.com>
2025-05-11 22:45:07 +0530

    Add files via upload

---
+++ Main98.c
@@ -0,0 +1,50 @@
+// Write a structure capable of storing date. Write a function to compare those dates? Solve problem for time using ‘typedef’ keyword.
+#include <stdio.h>
+#include <conio.h>
+typedef struct Date
+{
+    int MM;
+    int DD;
+    int YYYY;
+} DT;
+int compare(DT d1, DT d2)
+{
+    // If d1 is in the future, return 1.
+    if ((d1.YYYY == d2.YYYY) && (d1.MM == d2.MM) && (d1.DD == d2.DD))
+    {
+        return 0;
+    }
+    if (d1.YYYY > d2.YYYY)
+    {
+        return 1;
+    }
+    if (d1.YYYY < d2.YYYY)
+    {
+        return -1;
+    }
+    else if (d1.MM > d2.MM)
+    {
+        return 1;
+    }
+    else if (d1.MM < d2.MM)
+    {
+        return -1;
+    }
+    else if (d1.DD > d2.DD)
+    {
+        return 1;
+    }
+    else if (d1.DD < d2.DD)
+    {
+        return -1;
+    }
+}
+int main()
+{
+    DT d1 = {9, 6, 2025};
+    DT d2 = {9, 6, 2005};
+    system("cls");
+    printf("%d", compare(d1, d2));
+    getch();
+    return 0;
+}
